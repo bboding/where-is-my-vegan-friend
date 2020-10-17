80271dcde0f95c3d15611ba633cd3a20b1b538f0
bokyeong <dingbboding@gmail.com>
2020-10-17 15:51:25 +0900

    프로젝트 이름 변경

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,38 @@
 import Head from 'next/head'
 
 export default function Home() {
+  const users = [
+    {
+      nickname: '기매용',
+      image: '',
+      fullAddress: ['서울시', '양천구', '목동'],
+      tags: '동네친구사귀기',
+      kakaoLink: '',
+    },
+    {
+      nickname: '나무',
+      image: '',
+      fullAddress: ['서울시', '양천구', '목동'],
+      tags: '정보공유',
+      kakaoLink: '',
+    },
+  ]
   return (
     <div className='container'>
       <Head>
         <title>where is my vegan friend</title>
-        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
         <h1>where is my vegan friend</h1>
+        {users.map((user) => (
+          <div className='profile'>
+            <div>{user.nickname}</div>
+            {user.fullAddress.map((address) => (
+              <span>{address} </span>
+            ))}
+          </div>
+        ))}
       </main>
 
       <style jsx>{`
@@ -20,6 +43,14 @@ export default function Home() {
             flex-direction: column;
             justify-content: center;
             align-items: center;
+            background-color: green;
+            border: solid 5px;
+          }
+
+          .profile {
+            border: solid 1px #ccc;
+            padding: 10px;
+            margin: 5px;
           }
         }
       `}</style>
